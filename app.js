09bd4a43f4cb5324683a7ee378ac5a49976f5332
Arturo Gonzalez <arturogf@gmail.com>
2017-08-31 20:04:54 +0200

    testing new messages

--- app.js
+++ app.js
@@ -26,5 +26,7 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-	session.send("Has dicho: %s", session.message.text);
+	session.send("¡Buenos días INNOVADOR(A)! Soy innobot, el chatbot de la Unidad de Innovación del Hospital Clínico San Carlos");
+	session.send("Si tienes una idea original u proyecto innovador mejoraría la calidad de vida, la vivencia de la enfermedad, la experiencia asistencial o cualquier otro aspecto, cuéntanosla. Este es tu lugar. Trabajamos para ti.")
+	//session.send("Has dicho: %s", session.message.text);
     });
